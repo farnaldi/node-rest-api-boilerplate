e4f6a37b97c538ed971da61b99c30106939ee180
farnaldi <arnaldifrancisco@gmail.com>
2020-01-21 21:36:54 -0300

    type cast all env data

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,14 +2,15 @@ import * as dotEnv from 'dotenv';
 
 dotEnv.config();
 
-export const { ALLOWED_ORIGINS } = process.env;
-export const { API_URL } = process.env;
-export const { API_PORT } = process.env;
-export const { DB_HOST } = process.env;
-export const { DB_USER } = process.env;
-export const { DB_PASSWORD } = process.env;
-export const { DB_NAME } = process.env;
+export const NODE_ENV = String(process.env.NODE_ENV);
+export const ALLOWED_ORIGINS = String(process.env.ALLOWED_ORIGINS);
+export const API_URL = String(process.env.API_URL);
+export const API_PORT = Number(process.env.API_PORT);
+export const DB_HOST = String(process.env.DB_HOST);
+export const DB_USER = String(process.env.DB_USER);
+export const DB_PASSWORD = String(process.env.DB_PASSWORD);
+export const DB_NAME = String(process.env.DB_NAME);
 export const DB_PORT = Number(process.env.DB_PORT);
-export const { JWT_SECRET } = process.env;
-export const { JWT_EXPIRATION } = process.env;
-export const PWD_SALT_ROUNDS = Number(process.env);
+export const JWT_SECRET = String(process.env.JWT_SECRET);
+export const JWT_EXPIRATION = String(process.env.JWT_EXPIRATION);
+export const PWD_SALT_ROUNDS = Number(process.env.PWD_SALT_ROUNDS);
